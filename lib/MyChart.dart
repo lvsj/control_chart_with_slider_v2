02c9823d91c57b500405e45c651694bddeaf26b4
lvshaojun <lvshaojun@cmhi.chinamobile.com>
2020-04-22 15:11:50 +0800

    OCT change piechart using global variable.

--- lib/MyChart.dart
+++ lib/MyChart.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:pie_chart/pie_chart.dart';
 
+_MyChartState chartState;
+
 class MyChart extends StatefulWidget {
   @override
-  _MyChartState createState() => _MyChartState();
+  _MyChartState createState() {
+    chartState=_MyChartState();
+    return chartState;
+  }
+  static Map<String, double> createData(double stateManagementValue){
+    Map<String, double> dataMap=Map();
+    dataMap.putIfAbsent("State Management", () => stateManagementValue);
+    dataMap.putIfAbsent("UI programming", () => 0.3);
+    dataMap.putIfAbsent("UX", () => 0.1);
+    dataMap.putIfAbsent("Backend", () => 0.2);
+    return dataMap;
+  }
 }
 
 class _MyChartState extends State<MyChart> {
-  Map<String, double> dataMap = Map();
+  Map<String, double> dataMap = MyChart.createData(0.1);
   List<Color> colorList = [
     Colors.red,
     Colors.green,
@@ -16,20 +29,10 @@ class _MyChartState extends State<MyChart> {
   ];
 
   @override
-  void initState() {
-    super.initState();
-    dataMap.putIfAbsent("Flutter", () => 0.1);
-    dataMap.putIfAbsent("React", () => 0.1);
-    dataMap.putIfAbsent("Xamarin", () => 0.1);
-    dataMap.putIfAbsent("Ionic", () => 0.1);
-  }
-
-  @override
   Widget build(BuildContext context) {
     return PieChart(
       dataMap: dataMap,
       chartRadius: MediaQuery.of(context).size.width / 1.5,
-      showLegends: false,
       colorList: colorList,
     );
   }
--- lib/MySlider.dart
+++ lib/MySlider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'MyChart.dart';
 
 class MySlider extends StatefulWidget {
 
@@ -9,15 +10,19 @@ class MySlider extends StatefulWidget {
 }
 
 class _MySliderState extends State<MySlider> {
-  double value=0;
+  double _value=0;
   @override
   Widget build(BuildContext context) {
-    return Slider(value: this.value, onChanged:_changedListener);
+    return Slider(value: this._value, onChanged:_changedListener);
   }
 
   void _changedListener(double value){
     setState(() {
-      this.value=value;
+      this._value=value;
     });
+    chartState?.setState(() {
+      chartState.dataMap=MyChart.createData(value);
+    });
+
   }
 }
